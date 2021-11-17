3aa9c60a5d984c7ce30132a991b5bd4ff6f45a54
medigory33 <49507183+medigory33@users.noreply.github.com>
2021-11-17 14:58:58 +0100

    final updates

---
+++ langton-ant/js/main.js
@@ -0,0 +1,10 @@
+import { setPixel } from './utils/canvas.js'
+
+const width = 128
+const height = 128
+
+const canvas = document.querySelector('canvas')
+canvas.width = width
+canvas.height = height
+
+setPixel(10, 10, 'red')
